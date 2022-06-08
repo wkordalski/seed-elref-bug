165d6dfbe0f48825c7d25fe3b37bada6b8d38625
Wojciech Kordalski <kordalski.wojciech@gmail.com>
2022-06-08 14:16:26 +0200

    Change WebSockets to Timers

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 use std::rc::Rc;
 use std::task::Waker;
 
-use connection::Connection;
+use gloo_timers::future::TimeoutFuture;
 use measurer::Measurer;
+use seed::div;
 use seed::prelude::*;
 
-use seed::div;
-
-mod connection;
 mod measurer;
 
 struct Model {
-    connection: Connection,
     measurer: Measurer,
     counter: u64,
 }
@@ -19,13 +16,11 @@ struct Model {
 enum Msg {
     AddRenderable,
     Measurer(measurer::Msg),
-    Connection(connection::Msg),
     Wake(Vec<Waker>),
 }
 
 fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Model {
     let msg_sender = orders.msg_sender();
-    let connection = Connection::new("wss://ws.postman-echo.com/raw", &mut orders.proxy(Msg::Connection));
     let measurer = Measurer::new(Rc::new({
         let outer = Rc::clone(&msg_sender);
         move |msg| outer(Some(Msg::Measurer(msg)))
@@ -33,7 +28,6 @@ fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Model {
 
     Model {
         counter: 0,
-        connection,
         measurer,
     }
 }
@@ -42,16 +36,15 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::AddRenderable => {
             let measurer = model.measurer.clone();
-            let connection = model.connection.clone();
             let id = model.counter;
             model.counter = model.counter.wrapping_add(1);
 
             orders.perform_cmd(async move {
                 for i in 0..4 {
                     let mr = measurer.clone();
-                    let connection = connection.clone();
                     wasm_bindgen_futures::spawn_local(async move {
-                        let text = connection.request(&format!("Message {id}/{i}")).await;
+                        let text = format!("Message {id}/{i}");
+                        TimeoutFuture::new(10).await;
                         seed::log!("Got content: ", text);
                         let r = format!("Renderable: {text}");
                         let ms = mr.measure(r).await;
@@ -61,7 +54,6 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
                 }
             });
         }
-        Msg::Connection(msg) => Connection::update(msg, &mut model.connection, &mut orders.proxy(Msg::Connection)),
         Msg::Measurer(msg) => model.measurer.update(msg, orders, Msg::Measurer),
         Msg::Wake(wakers) => {
             for w in wakers {
